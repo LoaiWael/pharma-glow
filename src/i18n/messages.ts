f7803e69039f66a2909fcd0c4673e843c39ecefd
Loai Wael <loaiwael.dev@gmail.com>
2026-08-18 17:00:24 +0300

    FIX build error

--- src/i18n/messages.ts
+++
@@ -1,8 +0,0 @@
-import { ar } from './messages/ar'
-import { en } from './messages/en'
-import type { Locale } from './locales'
-
-export const messages = { en, ar } as const
-
-export const getMessages = (locale: Locale): Record<string, string> =>
-  messages[locale] as unknown as Record<string, string>
